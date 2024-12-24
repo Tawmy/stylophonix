96a7093ddddd7be004be25f3072e940fbf77966f
Tawmy <git@tawmy.dev>
2024-12-24 13:11:00 +0100

    Add MarkupString support to news page

--- Stylophonix/Data/NavOption.cs
+++ Stylophonix/Data/NavOption.cs
@@ -2,11 +2,14 @@ using Stylophonix.Shared;
 
 namespace Stylophonix.Data;
 
-public record NavOption(string Text, string IconUrl, IEnumerable<PopupMenuOption>? Options,
+public record NavOption(
+    string Text,
+    string IconUrl,
+    IEnumerable<PopupMenuOption>? Options,
     string? DestinationUrl = null)
 {
     public NavOption(string Text, string IconUrl, string? DestinationUrl = null) : this(Text, IconUrl,
-        new[] {new PopupMenuOption(Text, true)}, DestinationUrl)
+        new[] { new PopupMenuOption(Text, true) }, DestinationUrl)
     {
     }
 
--- Stylophonix/Data/PopupMenuOption.cs
+++ Stylophonix/Data/PopupMenuOption.cs
@@ -1,4 +1,8 @@
 namespace Stylophonix.Data;
 
-public record PopupMenuOption(string Title, bool Bold, string? Subtitle = null, string? Url = null,
+public record PopupMenuOption(
+    string Title,
+    bool Bold,
+    string? Subtitle = null,
+    string? Url = null,
     string? ThumbnailUrl = null);
